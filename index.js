086c3409156b99c5d9c13334a1a888360c0c4811
Eric McKinney <mckinney99@gmail.com>
2018-02-27 21:01:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+class Driver(){
+  constructor(name){
+    this.name = name
+  }
+  startDate(){
+    new Date(year, month [, day [, hours [, minutes [, seconds [, milliseconds]]]]]);
+  }
+}
 
+class Route(){
+  
+}
