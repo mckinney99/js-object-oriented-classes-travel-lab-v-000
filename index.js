42d22a019660d6d85aedfb479476ee554d47770d
Eric McKinney <mckinney99@gmail.com>
2018-02-27 21:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ class Driver {
     this.name = name
     this.startDate = new Date(startDate)
   }
+
+  yearsExperienceFromBeginningOf(year){
+    return (year - this.startDate.getfullyear())
+  }
 }
+
